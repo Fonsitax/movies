58a357694b0544ae67789bf5520d9f6732e2129a
rashin2604 <45271614+rashin2604@users.noreply.github.com>
2024-06-19 13:17:47 +0200

    favBtn and Notedit (#21)

--- journal.js
+++ journal.js
@@ -1,5 +1,5 @@
 import { updateUI } from './src/journalUi.js';
 
-const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+export const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
 
 updateUI(favorites);
